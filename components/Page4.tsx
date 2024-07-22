2b26632fce6ad6cc02a35c7f2f579469e92fd862
Abhirup Pan <abhiruppan2016@gmail.com>
2024-07-22 23:46:32 +0530

    commit

--- components/Page4.tsx
+++ components/Page4.tsx
@@ -4,8 +4,8 @@ function Page4() {
   return (
     <div className=' flex gap-[10vw] items-center px-[7vw] py-[5vh] bg-gradient-to-r from-[#fabc1f] to-[#7da545] h-[40vh] w-screen'>
       <div className=' text-white'>
-         <h1 className='text-3xl w-[20vw] my-[5vh]'>Let's discuss Your Project</h1>
-         <p className=''>Let's figure out how to create an effective application, its cost and terms of its development</p>
+         <h1 className='text-3xl w-[20vw] my-[5vh]'>Lets discuss Your Project</h1>
+         <p className=''>Lets figure out how to create an effective application, its cost and terms of its development</p>
       </div >
       <div></div>
     </div>
